310e357a8d75fc465327df3495357418f168760a
HSDM10 <hsdm10@HSDM10deiMac.local>
2018-09-21 19:05:39 +0800

    miniprogram

---
+++ WechatDemo/Classes/Base/Helper/UITabBar+traitCollection.swift
@@ -0,0 +1,19 @@
+//
+//  LS.swift
+//  WechatDemo
+//
+//  Created by HSDM10 on 2018/9/17.
+//  Copyright © 2018年 linshengqi. All rights reserved.
+//
+
+import UIKit
+
+extension UITabBar {
+    //让图片和文字在iOS11下仍然保持上下排列
+    override open var traitCollection: UITraitCollection {
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            return UITraitCollection(horizontalSizeClass: .compact)
+        }
+        return super.traitCollection
+    }
+}
